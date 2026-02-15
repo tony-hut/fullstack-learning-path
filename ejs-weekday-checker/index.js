2dfee7b18cfd7d2fb873f28c3ff1b986a3ac4e30
tony-hut <anton.hut.dev@gmail.com>
2026-02-15 20:00:37 +0200

    Refactor: move logic to request scope and cleanup code

--- ejs-weekday-checker/index.js
+++ ejs-weekday-checker/index.js
@@ -1,16 +1,14 @@
 import express from "express";
-import ejs from "ejs";
-
 
 const app = express();
 const port = 3000;
-const d = new Date();
-let advise = "";
 
 app.get("/", (req, res) => {
+  const d = new Date();
   let day = d.getDay();
-  if (day === 6 || day == 0) {
-    advise = "Hey! It's a weekend, it's time to fun!"
+  let advise = "";
+  if (day === 6 || day === 0) {
+    advise = "Hey! It's a weekend, it's time to have fun!"
   } else {
     advise = "Hey! It's a weekday, it's time to work hard!"
   }
